a7ebf0a86b14d7cdadbea27b8013cf46f7b7f27b
superfastscyphozoa <mitchellelhage@gmail.com>
2026-02-25 19:51:41 +1100

    commit lol

---
+++ src/main/java/net/superfastscyphozoa/chellsmod/Chellsmod.java
@@ -0,0 +1,10 @@
+package net.superfastscyphozoa.chellsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Chellsmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/superfastscyphozoa/chellsmod/client/ChellsmodClient.java
@@ -0,0 +1,10 @@
+package net.superfastscyphozoa.chellsmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChellsmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
